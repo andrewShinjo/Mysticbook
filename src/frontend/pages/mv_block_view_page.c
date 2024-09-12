39a5d0c74d7b9aa79dc9c56c69c537652bfafa41
Andrew Shinjo <shinjo1233@gmail.com>
2024-09-12 20:11:30 +0800

    save work

--- src/frontend/pages/mv_block_view_page.c
+++ src/frontend/pages/mv_block_view_page.c
@@ -1,4 +1,5 @@
 #include "./mb_block_view_page.h"
+#include "../components/mb_text_view.h"
 
 static void mb_block_view_page_dispose(GObject *object);
 static void mb_block_view_page_finalize(GObject *object);
@@ -6,6 +7,7 @@ static void mb_block_view_page_finalize(GObject *object);
 struct _MbBlockViewPage
 {
 	GtkWidget parent;
+  GtkWidget *vbox;
 };
 
 G_DEFINE_TYPE(MbBlockViewPage, mb_block_view_page, GTK_TYPE_WIDGET)
@@ -14,10 +16,33 @@ G_DEFINE_TYPE(MbBlockViewPage, mb_block_view_page, GTK_TYPE_WIDGET)
 // * Widget lifecycle
 // *********************************************************************
 
-static void mb_block_view_page_init(MbBlockViewPage *self) {}
-static void mb_block_view_page_class_init(MbBlockViewPageClass *klass) {}
-static void mb_block_view_page_dispose(GObject *object) {}
-static void mb_block_view_page_finalize(GObject *object) {}
+static void mb_block_view_page_dispose(GObject *object) 
+{
+  MbBlockViewPage *self = MB_BLOCK_VIEW_PAGE(object);
+  g_clear_pointer(&self->vbox, gtk_widget_unparent);
+  G_OBJECT_CLASS(mb_block_view_page_parent_class)->dispose(object);
+}
+
+static void mb_block_view_page_finalize(GObject *object)
+{
+  G_OBJECT_CLASS(mb_block_view_page_parent_class)->finalize(object);
+}
+
+static void mb_block_view_page_init(MbBlockViewPage *self)
+{
+  self->vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);  
+  gtk_widget_set_hexpand(self->vbox, TRUE);
+  gtk_widget_set_vexpand(self->vbox, TRUE);
+
+  gtk_widget_set_parent(self->vbox, GTK_WIDGET(self));
+}
+static void mb_block_view_page_class_init(MbBlockViewPageClass *klass) 
+{
+  GObjectClass *object_class = G_OBJECT_CLASS(klass);
+  object_class->dispose = mb_block_view_page_dispose;
+  object_class->finalize = mb_block_view_page_finalize;
+  gtk_widget_class_set_layout_manager_type(GTK_WIDGET_CLASS(klass), GTK_TYPE_BOX_LAYOUT);
+}
 
 // *********************************************************************
 // * Public
