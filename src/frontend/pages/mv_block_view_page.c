4e8aa011ce98985b99a8aef1c6408f80864d0402
Andrew Shinjo <shinjo1233@gmail.com>
2024-10-06 22:42:48 -0700

    got box select painted with snapshot override

--- src/frontend/pages/mv_block_view_page.c
+++ src/frontend/pages/mv_block_view_page.c
@@ -17,6 +17,7 @@ struct _MbBlockViewPage
   GtkWidget *root_block;
 
   // Drag
+  GtkGesture *gesture_drag;
   double x0;
   double x1;
   double y0;
@@ -36,6 +37,11 @@ drag_begin(
   gpointer user_data
 )
 {
+  GtkWidget *self = GTK_WIDGET(user_data);
+  MbBlockViewPage *_self = MB_BLOCK_VIEW_PAGE(user_data);
+  g_print("drag_begin\n");
+  _self->x0 = start_x;
+  _self->y0 = start_y;
 }
 
 static void
@@ -46,6 +52,13 @@ drag_update(
   gpointer user_data
 )
 {
+  GtkWidget *self = GTK_WIDGET(user_data);
+  MbBlockViewPage *_self = MB_BLOCK_VIEW_PAGE(user_data);
+  g_print("drag_update\n");
+  _self->dragging = TRUE;
+  _self->x1 = offset_x;
+  _self->y1 = offset_y;
+  gtk_widget_queue_draw(self);
 }
 
 static void
@@ -56,6 +69,15 @@ drag_end(
   gpointer user_data
 )
 {
+  GtkWidget *self = GTK_WIDGET(user_data);
+  MbBlockViewPage *_self = MB_BLOCK_VIEW_PAGE(user_data);
+  g_print("drag_end\n");
+  _self->x0 = 0;
+  _self->x1 = 0;
+  _self->y0 = 0;
+  _self->y1 = 0;
+  _self->dragging = FALSE;
+  gtk_widget_queue_draw(self);
 }
 
 gboolean 
@@ -85,14 +107,19 @@ mb_block_view_page_snapshot(GtkWidget *widget, GtkSnapshot *snapshot)
   MbBlockViewPage *_self = MB_BLOCK_VIEW_PAGE(widget);
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS(mb_block_view_page_parent_class);
 
-  // Call parent snapshot function.
   widget_class->snapshot(widget, snapshot);
 
-  g_print("Draw rectangle.\n");
-  graphene_rect_t graphene_rect = GRAPHENE_RECT_INIT(0, 0, 100, 100);
-  GdkRGBA color;
-  gdk_rgba_parse(&color, "red");
-  gtk_snapshot_append_color(snapshot, &color, &graphene_rect);
+  if(_self->dragging)
+  {
+    g_print("Drawing rectangle.\n");
+    g_print("x0=%f, y0=%f, x1=%f, y1=%f\n", _self->x0, _self->y0, _self->x1, _self->y1);
+    graphene_rect_t graphene_rect = GRAPHENE_RECT_INIT(
+        _self->x0, _self->y0, _self->x1, _self->y1
+    );
+    GdkRGBA color;
+    gdk_rgba_parse(&color, "red");
+    gtk_snapshot_append_color(snapshot, &color, &graphene_rect);
+  }
 }
 
 static void 
@@ -111,6 +138,9 @@ mb_block_view_page_init(MbBlockViewPage *self)
   gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(self->scrolled_window), self->layout);
   gtk_box_append(GTK_BOX(self->layout), self->root_block);
   gtk_widget_set_parent(self->scrolled_window, GTK_WIDGET(self));
+
+  self->gesture_drag = gtk_gesture_drag_new();
+  gtk_widget_add_controller(GTK_WIDGET(self), GTK_EVENT_CONTROLLER(self->gesture_drag));
   
   self->x0 = 0;
   self->x1 = 0;
@@ -118,28 +148,9 @@ mb_block_view_page_init(MbBlockViewPage *self)
   self->y1 = 0;
   self->dragging = FALSE;
 
-  /*
-  g_signal_connect(
-    self->gesture_drag, 
-    "drag-begin", 
-    G_CALLBACK(drag_begin), 
-    self
-  );
-
-  g_signal_connect(
-    self->gesture_drag,
-    "drag-update",
-    G_CALLBACK(drag_update),
-    self
-  );
-
-  g_signal_connect(
-    self->gesture_drag, 
-    "drag-end", 
-    G_CALLBACK(drag_end), 
-    self
-  );
-  */
+  g_signal_connect(self->gesture_drag, "drag-begin", G_CALLBACK(drag_begin), self);
+  g_signal_connect(self->gesture_drag, "drag-update", G_CALLBACK(drag_update), self);
+  g_signal_connect(self->gesture_drag, "drag-end", G_CALLBACK(drag_end), self);
 }
 static void mb_block_view_page_class_init(MbBlockViewPageClass *klass) 
 {
