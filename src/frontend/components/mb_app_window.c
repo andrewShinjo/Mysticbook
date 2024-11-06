813964dbb39664448ee8c26f36e1a36a09e37847
Andrew Shinjo <shinjo1233@gmail.com>
2024-11-05 22:13:41 -0800

    added home button

--- src/frontend/components/mb_app_window.c
+++ src/frontend/components/mb_app_window.c
@@ -15,7 +15,17 @@ struct _MbAppWindow
 };
 G_DEFINE_TYPE(MbAppWindow, mb_app_window, GTK_TYPE_APPLICATION_WINDOW)
 /* FORWARD DECLARATION */
+static void open_document_cb(MbDocumentsPage *documents_page, gpointer user_data);
 /* CALLBACK */
+static void go_home(GtkButton *button, gpointer user_data)
+{
+  MbAppWindow *_self = MB_APP_WINDOW(user_data);
+  gtk_widget_unparent(_self->active_page);
+  _self->active_page = mb_documents_page_new();
+  gtk_box_append(GTK_BOX(_self->horizontal_box), _self->active_page);
+  gtk_widget_set_hexpand(_self->active_page, TRUE);
+  g_signal_connect(_self->active_page, "open_doc", G_CALLBACK(open_document_cb), _self);
+}
 static void open_document_cb(MbDocumentsPage *documents_page, gpointer user_data)
 {
   GtkWidget *self = GTK_WIDGET(user_data);
@@ -24,7 +34,8 @@ static void open_document_cb(MbDocumentsPage *documents_page, gpointer user_data
   g_object_get(G_OBJECT(documents_page), "id", &id, NULL);
   gtk_widget_unparent(_self->active_page);
   _self->active_page = mb_block_view_page_new(id);
-  gtk_window_set_child(GTK_WINDOW(self), _self->active_page);
+  gtk_box_append(GTK_BOX(_self->horizontal_box), _self->active_page);
+  gtk_widget_set_hexpand(_self->active_page, TRUE);
   gtk_widget_allocate(_self->active_page, 0, 0, 0, NULL);
 }
 /* PROPERTIES */
@@ -46,6 +57,7 @@ static void mb_app_window_init(MbAppWindow *self)
   gtk_window_set_child(window, self->horizontal_box);
   /* CONNECT TO SIGNALS */
   g_signal_connect(self->active_page, "open_doc", G_CALLBACK(open_document_cb), self);
+  g_signal_connect(self->home_button, "clicked", G_CALLBACK(go_home), self);
 }
 static void
 mb_app_window_class_init(MbAppWindowClass *klass) {}
