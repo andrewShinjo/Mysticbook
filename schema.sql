4ce9884e410691674a295f1c04447fc3d67dbed7
Andrew Shinjo <shinjo1233@gmail.com>
2024-11-11 21:43:24 -0800

    added update and delete trigger, added id

--- schema.sql
+++ schema.sql
@@ -9,11 +9,13 @@ CREATE TABLE IF NOT EXISTS blocks(
 	content           TEXT
 );
 
-CREATE VIRTUAL TABLE IF NOT EXISTS blocks_fts5 using fts5(content);
+CREATE VIRTUAL TABLE IF NOT EXISTS blocks_fts5 using fts5(id, content);
 
-CREATE TRIGGER IF NOT EXISTS blocks_insert_trigger
-AFTER INSERT ON blocks
-FOR EACH ROW
-  BEGIN
-    INSERT INTO blocks_fts5 (content) VALUES (new.content);
-  END;
+CREATE TRIGGER IF NOT EXISTS blocks_insert_trigger AFTER INSERT ON blocks
+FOR EACH ROW BEGIN INSERT INTO blocks_fts5 (id, content) VALUES (NEW.id, NEW.content); END;
+
+CREATE TRIGGER IF NOT EXISTS blocks_update_trigger AFTER UPDATE ON blocks 
+BEGIN UPDATE blocks_fts5 SET id = NEW.id, content = NEW.content WHERE id = NEW.id; END;
+
+CREATE TRIGGER IF NOT EXISTS blocks_delete_trigger AFTER DELETE ON blocks
+BEGIN DELETE FROM blocks_fts WHERE id = OLD.id; END;
