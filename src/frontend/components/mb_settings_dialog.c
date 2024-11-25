8fdf382bd729c75122cf708524adf1ad97eeba59
Andrew Hikaru Shinjo <shinjo1233@gmail.com>
2024-11-24 19:44:53 -0800

    Implemented mb_settings_dialog_init

--- src/frontend/components/mb_settings_dialog.c
+++ src/frontend/components/mb_settings_dialog.c
@@ -13,7 +13,10 @@ G_DEFINE_TYPE(MbSettingsDialog, mb_settings_dialog, GTK_TYPE_WINDOW)
 /* Properties */
 /* Signals */
 /* Widget lifecycle */
-static void mb_settings_dialog_init(MbSettingsDialog *_self) {}
+static void mb_settings_dialog_init(MbSettingsDialog *_self) 
+{
+
+}
 static void mb_settings_dialog_class_init(MbSettingsDialogClass *klass) {}
 static void mb_settings_dialog_dispose(GObject *object) {}
 static void mb_settings_dialog_finalize(GObject *object) {}
