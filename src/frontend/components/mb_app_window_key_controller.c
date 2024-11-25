754911ef373f50a5d5ad233bf5d81eabbac98576
Andrew Hikaru Shinjo <shinjo1233@gmail.com>
2024-11-24 23:45:12 -0800

    save

--- src/frontend/components/mb_app_window_key_controller.c
+++ src/frontend/components/mb_app_window_key_controller.c
@@ -3,9 +3,11 @@
 static gboolean
 key_pressed(GtkEventControllerKey* self, guint keyval, guint keycode, GdkModifierType state, gpointer user_data)
 {
-	if((state & GDK_CONTROL_MASK) && keyval == GDK_KEY_o)
+	gboolean ctrl_o_pressed = (state & GDK_CONTROL_MASK) && keyval == GDK_KEY_o;
+
+	if(ctrl_o_pressed)
 	{
-		g_print("Ctrl+O pressed.\n");
+		g_print("Ctrl+o pressed.\n");
 		return TRUE;
 	}
 	return FALSE;
@@ -15,6 +17,6 @@ GtkEventController* mb_app_window_key_new(MbAppWindow *app_window)
 {
 	GtkEventController *self = gtk_event_controller_key_new();
 	gtk_widget_add_controller(GTK_WIDGET(app_window), self);
-	g_signal_connect(self, "key-pressed", G_CALLBACK(key_pressed), NULL);
+	g_signal_connect(self, "key-pressed", G_CALLBACK(key_pressed), app_window);
 	return self;
 }
