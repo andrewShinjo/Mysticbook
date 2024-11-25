d8d28c240af17ce48c37cfdb19e85be0700ebb04
Andrew Hikaru Shinjo <shinjo1233@gmail.com>
2024-11-25 15:58:35 -0800

    added entry

--- src/frontend/components/mb_block_search.c
+++ src/frontend/components/mb_block_search.c
@@ -21,7 +21,9 @@ GtkWidget* mb_block_search_open()
 		gtk_window_set_child(GTK_WINDOW(window), vbox);
 
 		label = gtk_label_new("Search for a block.");
+		entry = gtk_entry_new();
 		gtk_box_append(GTK_BOX(vbox), label);
+		gtk_box_append(GTK_BOX(vbox), entry);
 	}
 
 	if(!gtk_widget_get_visible(window))
